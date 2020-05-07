b9ef0e9b327b730962973e138ded32a62fb15db1
@ <yuezhaoyang7449@163.com>
2020-05-07 20:09:16 +0800

    Update api

--- library/src/main/java/androidx/kotlin/expand/app/ActivityX.kt
+++ library/src/main/java/androidx/kotlin/expand/app/ActivityX.kt
@@ -10,8 +10,10 @@ import android.net.Uri
 import android.util.DisplayMetrics
 import android.view.View
 import android.view.WindowManager
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.kotlin.expand.annotation.Version
 import androidx.kotlin.expand.annotation.VersionLog
 import androidx.kotlin.expand.os.camera.CameraStatus
@@ -100,6 +102,15 @@ fun Activity.openCameraExpand(fileUri: Uri, video: Boolean): CameraStatus =
         video
     )
 
+@Version(
+    version = [Version.PINEAPPLE],
+    log = [
+        VersionLog(Version.PINEAPPLE, "add findFragmentByIdExpand")
+    ]
+)
+val AppCompatActivity.fragmentsExpand: MutableList<Fragment>
+    get() = supportFragmentManager.fragments
+
 @JvmName("findFragmentByTag")
 @Version(
     version = [Version.BANANA],
@@ -109,6 +120,17 @@ fun Activity.openCameraExpand(fileUri: Uri, video: Boolean): CameraStatus =
 )
 fun AppCompatActivity.findFragmentByTagExpand(tag: String, of: (fragment: Fragment?) -> Unit) {
     of.invoke(supportFragmentManager.findFragmentByTag(tag))
+}
+
+@JvmName("findFragmentById")
+@Version(
+    version = [Version.PINEAPPLE],
+    log = [
+        VersionLog(Version.PINEAPPLE, "add findFragmentByIdExpand")
+    ]
+)
+fun AppCompatActivity.findFragmentByIdExpand(@IdRes id: Int, of: (fragment: Fragment?) -> Unit) {
+    of.invoke(supportFragmentManager.findFragmentById(id))
 }
 
 @JvmName("findFragmentByTag")
@@ -123,44 +145,88 @@ fun <T : Fragment> AppCompatActivity.findFragmentByTagExpand(
     ifNone: (String) -> T
 ): T = supportFragmentManager.findFragmentByTag(tag) as T? ?: ifNone(tag)
 
-@JvmName("showFragment")
+@JvmName("runOnCommit")
 @Version(
-    version = [Version.BANANA],
+    version = [Version.PINEAPPLE],
     log = [
-        VersionLog(Version.BANANA, "init submit")
+        VersionLog(Version.PINEAPPLE, "add runOnCommit")
     ]
 )
-fun AppCompatActivity.showFragmentExpand(fragment: Fragment): Int =
-    supportFragmentManager.beginTransaction().show(fragment).commitAllowingStateLoss()
+fun AppCompatActivity.showRunOnCommitExpand(
+    fragment: Fragment,
+    runnable: Runnable
+): FragmentTransaction =
+    supportFragmentManager.beginTransaction().show(fragment).runOnCommit(runnable)
+
+@JvmName("showFragment")
+@Version(
+    version = [Version.BANANA, Version.PINEAPPLE],
+    log = [
+        VersionLog(Version.BANANA, "init submit"),
+        VersionLog(Version.PINEAPPLE, "add commit type")
+    ]
+)
+fun AppCompatActivity.showFragmentExpand(
+    fragmentType: FragmentType = FragmentType.COMMIT_ALLOWING_STATE_LOSS,
+    fragment: Fragment
+) = supportFragmentManager.beginTransaction().show(fragment).commitExpand(fragmentType)
 
 @JvmName("hideFragment")
 @Version(
-    version = [Version.BANANA],
+    version = [Version.BANANA, Version.PINEAPPLE],
     log = [
-        VersionLog(Version.BANANA, "init submit")
+        VersionLog(Version.BANANA, "init submit"),
+        VersionLog(Version.PINEAPPLE, "add commit type")
     ]
 )
-fun AppCompatActivity.hideFragmentExpand(fragment: Fragment): Int =
-    supportFragmentManager.beginTransaction().hide(fragment).commitAllowingStateLoss()
+fun AppCompatActivity.hideFragmentExpand(
+    fragmentType: FragmentType = FragmentType.COMMIT_ALLOWING_STATE_LOSS,
+    fragment: Fragment
+) = supportFragmentManager.beginTransaction().hide(fragment).commitExpand(fragmentType)
 
 @JvmName("addFragment")
 @Version(
-    version = [Version.BANANA],
+    version = [Version.BANANA, Version.PINEAPPLE],
     log = [
-        VersionLog(Version.BANANA, "init submit")
+        VersionLog(Version.BANANA, "init submit"),
+        VersionLog(Version.PINEAPPLE, "add commit type")
     ]
 )
-fun AppCompatActivity.addFragmentExpand(id: Int, fragment: Fragment): Int =
-    supportFragmentManager.beginTransaction().add(id, fragment, fragment.javaClass.simpleName)
-        .commitAllowingStateLoss()
+fun AppCompatActivity.addFragmentExpand(
+    id: Int,
+    fragmentType: FragmentType = FragmentType.COMMIT_ALLOWING_STATE_LOSS,
+    fragment: Fragment
+) = supportFragmentManager.beginTransaction().add(id, fragment, fragment.javaClass.simpleName)
+    .commitExpand(fragmentType)
 
 @JvmName("replaceFragment")
 @Version(
-    version = [Version.BANANA],
+    version = [Version.BANANA, Version.PINEAPPLE],
     log = [
-        VersionLog(Version.BANANA, "init submit")
+        VersionLog(Version.BANANA, "init submit"),
+        VersionLog(Version.PINEAPPLE, "add commit type")
     ]
 )
-fun AppCompatActivity.replaceFragmentExpand(id: Int, fragment: Fragment): Int =
-    supportFragmentManager.beginTransaction().replace(id, fragment, fragment.javaClass.simpleName)
-        .commitAllowingStateLoss()
+fun AppCompatActivity.replaceFragmentExpand(
+    id: Int,
+    fragmentType: FragmentType = FragmentType.COMMIT_ALLOWING_STATE_LOSS,
+    fragment: Fragment
+) = supportFragmentManager.beginTransaction().replace(id, fragment, fragment.javaClass.simpleName)
+    .commitExpand(fragmentType)
+
+@Suppress("NOTHING_TO_INLINE")
+internal inline fun FragmentTransaction.commitExpand(fragmentType: FragmentType) {
+    when (fragmentType) {
+        FragmentType.COMMIT -> commit()
+        FragmentType.COMMIT_ALLOWING_STATE_LOSS -> commitAllowingStateLoss()
+        FragmentType.NOW -> commitNow()
+        FragmentType.NOW_ALLOWING_STATE_LOSS -> commitNowAllowingStateLoss()
+    }
+}
+
+enum class FragmentType {
+    COMMIT,
+    COMMIT_ALLOWING_STATE_LOSS,
+    NOW,
+    NOW_ALLOWING_STATE_LOSS
+}
